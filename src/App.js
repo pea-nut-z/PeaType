ee37bdf2874f8bd009c541b63895e5e14915b4f3
pea-nut-z <topeanutz@gmail.com>
2022-03-08 21:56:34 -0500

    added settings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import * as helper from "./helper";
-// import TimeButton from "./components/TimeButton";
 
 function App() {
   const [settings, setSettings] = useState(false);
   const [langData, setLangData] = useState(null);
   const [searchStr, setSearchStr] = useState("");
+  const [showLangList, setShowLangList] = useState(false);
   const [filteredLang, setfilteredLang] = useState(null);
-  const [time, setTime] = useState(15);
+  const [selectedLang, setSelectedLang] = useState("en");
+  const [selectedName, setSelectedName] = useState("English");
+  const [lastNode, setLastNode] = useState(0);
+  const [currentNode, setCurrentNode] = useState(0);
+  // const [hilight, setHilight] = useState(0);
+  const [selectedTime, setSelectedTime] = useState(15);
   const timeOptions = [15, 30, 60, 120, 240];
-  // console.log({ time });
+  console.log({ showLangList });
+  // console.log({ selectedName });
 
   useEffect(() => {
     try {
@@ -24,21 +30,72 @@ function App() {
     }
   }, []);
 
-  useEffect(() => {
-    if (searchStr.length === 0) {
-      setfilteredLang(null);
-    } else {
+  const closeLangList = () => {
+    setShowLangList(false);
+  };
+
+  const openLangList = () => {
+    setShowLangList(true);
+  };
+
+  const toggleSettings = () => {
+    setSettings(!settings);
+  };
+
+  const selectLang = (lang) => {
+    setSelectedLang(lang.language);
+    setSelectedName(lang.name);
+    setSearchStr(lang.name);
+    setCurrentNode(0);
+    closeLangList();
+    setfilteredLang(null);
+  };
+
+  const handleLangOnChange = (e) => {
+    const container = document.querySelector("#container");
+    const val = e.target.value;
+    setSearchStr(val);
+    if (val) {
       const matches = langData.filter((data) => {
-        const str = searchStr.toLowerCase();
+        const str = val.toLowerCase();
         const name = data.name.toLowerCase().substring(0, str.length);
         return str === name;
       });
       setfilteredLang(matches);
+      openLangList();
+      container.addEventListener("mousedown", closeLangList);
+    } else {
+      setfilteredLang(null);
+      closeLangList();
+      container.removeEventListener("mousedown", closeLangList);
     }
-  }, [searchStr]);
+  };
 
-  const toggleSettings = () => {
-    setSettings(!settings);
+  const handleLangOnKeyDown = (e) => {
+    const key = e.key;
+    const list = document.querySelectorAll("#filteredLang");
+
+    if (key === "ArrowDown" && filteredLang) {
+      list[currentNode].classList.add("hilight");
+
+      if (currentNode > 0) {
+        list[currentNode - 1].classList.remove("hilight");
+      } else {
+        list[list.length - 1].classList.remove("hilight");
+      }
+
+      if (currentNode === list.length - 1) {
+        setCurrentNode(0);
+      } else {
+        setCurrentNode(currentNode + 1);
+      }
+
+      setLastNode(currentNode);
+    }
+
+    if (key === "Enter" && showLangList) {
+      list[lastNode].click();
+    }
   };
 
   const renderSettings = () => {
@@ -46,33 +103,38 @@ function App() {
       <section>
         <h1>Settings</h1>
         <div>
-          <label htmlFor="languages">Languages</label>
+          <h4>Language</h4>
           <input
-            id="languages"
             type="text"
-            name="hidden"
-            placeholder="Find and change language"
+            autoComplete="false"
             value={searchStr}
-            onChange={(e) => {
-              setSearchStr(e.target.value);
-            }}
+            placeholder={`Enter a language to change from ${selectedName}.`}
+            onFocus={filteredLang && openLangList}
+            onChange={handleLangOnChange}
+            onKeyDown={handleLangOnKeyDown}
           />
           <div>
-            {filteredLang &&
-              filteredLang.map((lang) => {
-                return <div>{lang.name}</div>;
+            {showLangList &&
+              filteredLang.map((lang, idx) => {
+                return (
+                  <div id="filteredLang" key={idx} onClick={() => selectLang(lang)}>
+                    {lang.name}
+                  </div>
+                );
               })}
           </div>
         </div>
         <div>
           <h4>Time</h4>
           <div>
-            {timeOptions.map((option) => {
+            {timeOptions.map((option, idx) => {
               return (
                 <button
+                  key={idx}
                   type="button"
+                  className={selectedTime === option ? "selected-time-btn" : "time-btn"}
                   onClick={() => {
-                    setTime(option);
+                    setSelectedTime(option);
                   }}
                 >
                   {option}
@@ -81,12 +143,15 @@ function App() {
             })}
           </div>
         </div>
+        <button type="button" onClick={toggleSettings}>
+          OK
+        </button>
       </section>
     );
   };
 
   return (
-    <div>
+    <div id="container">
       <section>
         <header>PeaType</header>
         <button>Redo</button>
